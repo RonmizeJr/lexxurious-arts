0893b2e0af0514c833259e09f680ccb1328d2a39
Ronald Mize Jr <rwthojr5@gmail.com>
2023-03-30 09:15:02 -0500

    Setting up Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function Hero() {
-  return <div>Hero</div>;
+  return (
+    <div className='text-white'>
+      <div className='max-w-[800px]'></div>
+    </div>
+  );
 }
 
 export default Hero;
