c4e51a788b596b07219f9c0f24b9e4067e2e2e49
Ronald Mize Jr <rwthojr5@gmail.com>
2023-04-01 08:27:35 -0500

    Footer component Done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div className='bg-black text-center px-6 py-3'>
-      <h1>&copy; 2023, Lexxurious Arts, All rights reserved.</h1>
-    </div>
+    <footer class='text-2xl text-center text-white py-6 px-4'>
+      © 2023, Lexxurious Arts, All rights reserved.
+    </footer>
   );
 };
 
